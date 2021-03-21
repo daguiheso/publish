e4072664b83fa6a49afa5ec9fc1fcf55da84dfd7
Daniel H <danielhernandez16@hotmail.com>
2021-03-21 15:32:53 -0500

    refactor(webpack.config): eslint auto fixed

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
@@ -22,11 +22,11 @@ module.exports = {
       {
         test: /\.s[ac]ss$/i,
         use: [
-          "style-loader",
-          "css-loader",
-          "sass-loader",
-        ],
-      },
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
     ]
   },
   devServer: {
