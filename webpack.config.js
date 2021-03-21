55f4879eaf7dff340b3d8d3a59ad21494ca6543b
Daniel H <danielhernandez16@hotmail.com>
2021-03-21 13:32:04 -0500

    feat(eslint): installed and configured

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,18 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
 }
