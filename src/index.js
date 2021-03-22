c13d7190d90f7e8d6d50c575e2a2cddf08fe48d6
Daniel H <danielhernandez16@hotmail.com>
2021-03-21 21:00:16 -0500

    feat: index app was created

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('init')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './scss/index.scss'
+
+ReactDOM.render(
+  <h1>Hello world 3</h1>,
+  document.getElementById('root')
+)
