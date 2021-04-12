1c760c1b711cce7f503e55e1dceefa83dada2ada
Daniel H <danielhernandez16@hotmail.com>
2021-04-11 19:22:00 -0500

    tiny(app): styles app

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import NewPost from './components/NewPost/NewPost'
 import Post from './components/Post/Post'
 import PostsContainer from './containers/PostsContainer'
 
-function App() {
+import './App.scss'
+
+function App () {
   return (
-    <div>
-      <div className="container">
-        <section className="wrapper">
-          <NewPost />
-          <PostsContainer>
-            <Post />
-          </PostsContainer>
-        </section>
-      </div>
+    <div className="container">
+      <section className="wrapper">
+        <NewPost />
+        <PostsContainer>
+          <Post />
+        </PostsContainer>
+      </section>
     </div>
   )
 }
