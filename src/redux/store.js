3406352fe0960b4622526bd8d7e764fd2820ec97
Daniel H <danielhernandez16@hotmail.com>
2021-04-11 20:53:40 -0500

    feat(redux): use of redux-devtools-extension is enabled

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
-import { createStore } from 'redux'
+import { createStore, compose } from 'redux'
 import rootReducer from './reducers'
 
 const store = {
   posts: []
 }
 
-export default createStore(rootReducer, store)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+export default createStore(rootReducer, store, composeEnhancers())
